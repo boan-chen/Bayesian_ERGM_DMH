5f221cef6ac5feabd982faa997c33a86f03834b7
Bo-An Chen <seer2239@gmail.com>
2023-11-27 11:05:01 +0800

    restructure the code

--- python_ver1/DGP.py
+++ python_ver1/DGP.py
@@ -1,5 +1,6 @@
 # %%
 import numpy as np
+from tqdm import tqdm
 
 # Set the parameters
 sample = 1
@@ -9,7 +10,6 @@ N = 100
 beta = [-3, 1, 1.0, -1.0]
 sig2 = 0.5
 ltnt = 0
-
 
 # %%
 class ergm_generating_process:
@@ -33,7 +33,7 @@ class ergm_generating_process:
         return H
 
     def network_metropolis(self, sample, r=1000):
-        for s in range(sample):
+        for s in tqdm(range(sample)):
             H = self.naive_network()
             Wn = np.double(H > 0)
 
@@ -48,13 +48,30 @@ class ergm_generating_process:
                     mask, 1 - Wn, Wn
                 )  # replace elements in the upper triangle
                 Wn = np.triu(Wn) + np.triu(Wn, 1).T
-            print(
-                [
-                    np.sum(np.sum(Wn)),
-                    np.max(np.sum(Wn, axis=0)),
-                    np.max(np.sum(Wn, axis=1)),
-                ]
-            )
-
+                
             self.Wn.append(Wn)
         return self.Wn, self.Xn, self.Zn
+
+# %% test the convergence of the generated networks
+
+network_generator = ergm_generating_process(N, beta, sig2, ltnt)
+edges = []
+maximum_degree = []
+Wn, Xn, Zn = network_generator.network_metropolis(500) #generate 500 networks
+edges = []
+maximum_degree = []
+for i in range(500):
+    edges.append(np.sum(np.sum(Wn[i])))
+    maximum_degree.append(max(np.sum(Wn[i], axis=0)))
+import matplotlib.pyplot as plt
+# plot the distribution of the number of edges
+edges = np.array(edges)
+plt.hist(edges, bins = 25)
+plt.title("Distribution of the number of edges")
+# show the density plot
+import seaborn as sns
+sns.kdeplot(x = edges, y = maximum_degree, cmap="Blues", shade=True, shade_lowest=False)
+plt.xlabel("# of edges")
+plt.ylabel("Maximum degree")
+plt.title("Density plot of # of edges and the maximum degree")
+# %%
